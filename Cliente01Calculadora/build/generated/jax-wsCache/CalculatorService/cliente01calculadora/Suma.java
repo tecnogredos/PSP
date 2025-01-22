2b2c9625e7bcdd3334e8ca7f6ef8d1f07dff3972
tecnogredos <40858760+tecnogredos@users.noreply.github.com>
2025-01-22 16:43:21 +0100

    Add files via upload

---
+++ Cliente01Calculadora/build/generated/jax-wsCache/CalculatorService/cliente01calculadora/Suma.java
@@ -0,0 +1,71 @@
+
+package cliente01calculadora;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para suma complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="suma">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="val1" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *         &lt;element name="val2" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "suma", propOrder = {
+    "val1",
+    "val2"
+})
+public class Suma {
+
+    protected double val1;
+    protected double val2;
+
+    /**
+     * Obtiene el valor de la propiedad val1.
+     * 
+     */
+    public double getVal1() {
+        return val1;
+    }
+
+    /**
+     * Define el valor de la propiedad val1.
+     * 
+     */
+    public void setVal1(double value) {
+        this.val1 = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad val2.
+     * 
+     */
+    public double getVal2() {
+        return val2;
+    }
+
+    /**
+     * Define el valor de la propiedad val2.
+     * 
+     */
+    public void setVal2(double value) {
+        this.val2 = value;
+    }
+
+}
---
+++ Cliente01Calculadora/build/generated/jax-wsCache/CalculatorService/cliente01calculadora/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.medac.com/")
+package cliente01calculadora;
